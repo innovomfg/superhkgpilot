280a80ff7456192b3687293ecc7b3eaa69c10404
innovomfg <67807414+innovomfg@users.noreply.github.com>
2023-11-11 18:44:07 -0500

    Update values.py
    Signed-off-by: innovomfg <67807414+innovomfg@users.noreply.github.com>

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -15,8 +15,8 @@ Ecu = car.CarParams.Ecu
 
 
 class CarControllerParams:
-  ACCEL_MIN = -3.5 # m/s
-  ACCEL_MAX = 2.0 # m/s
+  ACCEL_MIN = -1.5 # m/s
+  ACCEL_MAX = 4.0 # m/s
 
   def __init__(self, CP, vEgoRaw=100.):
     self.STEER_DELTA_UP = 3
@@ -51,7 +51,7 @@ class CarControllerParams:
 
     # Default for most HKG
     else:
-      self.STEER_MAX = 384
+      self.STEER_MAX = 500
 
 
 class HyundaiFlags(IntFlag):
