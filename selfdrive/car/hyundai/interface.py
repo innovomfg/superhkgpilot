8e96597d6116ce08fd02d5ecbd2fdc02c960654a
innovomfg <67807414+innovomfg@users.noreply.github.com>
2023-11-11 18:43:05 -0500

    Update interface.py
    Signed-off-by: innovomfg <67807414+innovomfg@users.noreply.github.com>

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -93,7 +93,7 @@ class CarInterface(CarInterfaceBase):
     elif candidate == CAR.PALISADE:
       ret.mass = 1999.
       ret.wheelbase = 2.90
-      ret.steerRatio = 15.6 * 1.15
+      ret.steerRatio = 24.6
       ret.tireStiffnessFactor = 0.63
     elif candidate == CAR.ELANTRA:
       ret.mass = 1275.
