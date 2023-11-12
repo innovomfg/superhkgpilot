02c661016dee0239e8ffff079bf1a88c95e38a0b
dude241 <149417706+dude241@users.noreply.github.com>
2023-11-11 20:56:36 -0500

    Update carcontroller.py
    Signed-off-by: dude241 <149417706+dude241@users.noreply.github.com>

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -16,7 +16,7 @@ LongCtrlState = car.CarControl.Actuators.LongControlState
 
 # EPS faults if you apply torque while the steering angle is above 90 degrees for more than 1 second
 # All slightly below EPS thresholds to avoid fault
-MAX_ANGLE = 87
+MAX_ANGLE = 85
 MAX_ANGLE_FRAMES = 89
 MAX_ANGLE_CONSECUTIVE_FRAMES = 2
 
