5e71dd0a6d0ede436c13592298732b140d376240
innovomfg <james@innovomfg.com>
2024-06-16 18:45:10 -0400

    driver allowance changed back to 50

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -19,7 +19,7 @@ class CarControllerParams:
   def __init__(self, CP, vEgoRaw=100.):
     self.STEER_DELTA_UP = 3
     self.STEER_DELTA_DOWN = 7
-    self.STEER_DRIVER_ALLOWANCE = 75
+    self.STEER_DRIVER_ALLOWANCE = 50
     self.STEER_DRIVER_MULTIPLIER = 1
     self.STEER_DRIVER_FACTOR = 1
     self.STEER_THRESHOLD = 350
