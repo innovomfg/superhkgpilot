bd449499ea30844d86f74986b595c424a27a38f2
callal241 <40478165+callal241@users.noreply.github.com>
2024-12-18 14:20:09 -0500

    Update fingerprints.py
    ADDED RADAR AND TCU FINGERPRINT TO GENESIS_G80  FOR SUPPORT
    
    Signed-off-by: callal241 <40478165+callal241@users.noreply.github.com>

--- selfdrive/car/hyundai/fingerprints.py
+++ selfdrive/car/hyundai/fingerprints.py
@@ -483,19 +483,24 @@ FW_VERSIONS = {
   },
   CAR.GENESIS_G80: {
     (Ecu.fwdRadar, 0x7d0, None): [
+      b'\xf1\x10\x9f\x00\x00\x00',
+      b'\xf1\x00DH__ SCC FHCUP      1.00 1.01 96400-B1120         ',
       b'\xf1\x00DH__ SCC F-CU-      1.00 1.01 96400-B1110         ',
       b'\xf1\x00DH__ SCC F-CUP      1.00 1.01 96400-B1120         ',
       b'\xf1\x00DH__ SCC F-CUP      1.00 1.02 96400-B1120         ',
       b'\xf1\x00DH__ SCC FHCUP      1.00 1.01 96400-B1110         ',
     ],
     (Ecu.fwdCamera, 0x7c4, None): [
+      b'\xf1\x00DH  LKAS AT USA LHD 1.01 1.01 95895-B1500 161014',
       b'\xf1\x00DH  LKAS AT EUR LHD 1.01 1.01 95895-B1500 161014',
       b'\xf1\x00DH  LKAS AT KOR LHD 1.01 1.01 95895-B1500 161014',
       b'\xf1\x00DH  LKAS AT KOR LHD 1.01 1.02 95895-B1500 170810',
-      b'\xf1\x00DH  LKAS AT USA LHD 1.01 1.01 95895-B1500 161014',
       b'\xf1\x00DH  LKAS AT USA LHD 1.01 1.02 95895-B1500 170810',
       b'\xf1\x00DH  LKAS AT USA LHD 1.01 1.03 95895-B1500 180713',
       b'\xf1\x00DH  LKAS AT USA LHD 1.01 1.04 95895-B1500 181213',
+    ],
+    (Ecu.transmission, 0x7e1, None): [
+      b'\xf1\x00bcsh8p54  E18\x00\x00\x00\x00\x00\x00\x00SDH0T33NH3\x97\xe6\xbc\xb8',
     ],
   },
   CAR.GENESIS_G90: {
